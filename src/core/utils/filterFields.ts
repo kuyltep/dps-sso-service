f34fb4b4902ac888f73733c30b3984291dca1e2f
Vlad Petlyuk <vlad10112005@inbox.ru>
2025-02-14 11:25:44 +0300

    add utils functions

---
+++ src/core/utils/filterFields.ts
@@ -0,0 +1,12 @@
+export function filterFields<T extends object, K extends keyof T>(
+  obj: T,
+  keys: K[],
+): Pick<T, K> {
+  const result = {} as Pick<T, K>;
+  keys.forEach((key) => {
+    if (key in obj) {
+      result[key] = obj[key];
+    }
+  });
+  return result;
+}
