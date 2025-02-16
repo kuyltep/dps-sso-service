f8d810f8e2a177336fededa36535e6df04fb2169
Vlad Petlyuk <vlad10112005@inbox.ru>
2025-02-16 16:29:34 +0300

    add local strategies and decorators

---
+++ src/core/strategies/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '../services/config.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.getJwtSecret(),
+    });
+  }
+
+  async validate(payload: any) {
+    return { id: payload.sub, role: payload.role };
+  }
+}
