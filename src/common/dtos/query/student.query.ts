d9b2f7b512f4c3e1eff029239d81411df26f5ffa
Vlad Petlyuk <vlad10112005@inbox.ru>
2025-03-14 10:14:29 +0300

    update students dtos and enums for sortings by fields

--- src/common/dtos/query/student.query.ts
+++ src/common/dtos/query/student.query.ts
@@ -1,7 +1,15 @@
-import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 import { PageQueryDto } from './employee.query';
 import { Transform } from 'class-transformer';
 import { ApiProperty } from '@nestjs/swagger';
+import { StudentsOrderByEnum } from 'src/common/enums/student.order-by.enum';
+import { OrderTypeEnum } from 'src/common/enums/order-type.enum';
 
 export class StudentQueryDto extends PageQueryDto {
   @ApiProperty({ required: true })
@@ -24,4 +32,14 @@ export class StudentQueryDto extends PageQueryDto {
   @IsNumber()
   @IsOptional()
   study_year?: number;
+
+  @ApiProperty({ required: false, enum: StudentsOrderByEnum })
+  @IsEnum(StudentsOrderByEnum)
+  @IsOptional()
+  order_by: StudentsOrderByEnum = StudentsOrderByEnum.name;
+
+  @ApiProperty({ required: false, enum: OrderTypeEnum })
+  @IsOptional()
+  @IsEnum(OrderTypeEnum)
+  order: OrderTypeEnum = OrderTypeEnum.asc;
 }
---
+++ src/common/enums/student.order-by.enum.ts
@@ -0,0 +1,4 @@
+export enum StudentsOrderByEnum {
+  name = 'name',
+  created_at = 'created_at',
+}
