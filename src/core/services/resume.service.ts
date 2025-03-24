e2605907abe5222090b86abb9b14271b6724496c
Vlad Petlyuk <vlad10112005@inbox.ru>
2025-03-24 14:51:11 +0300

    update resumes service logic for delete resumes

--- src/core/services/resume.service.ts
+++ src/core/services/resume.service.ts
@@ -3,6 +3,7 @@ import { PrismaService } from './prisma.service';
 import { MinioService } from './minio.service';
 import { ProcessFileItemDto } from 'src/common/dtos/qdrant/priocess-file.dto';
 import { QdrantService } from './qdrant.service';
+import { ConfigService } from './config.service';
 
 @Injectable()
 export class ResumesService {
@@ -10,6 +11,7 @@ export class ResumesService {
     private readonly prismaService: PrismaService,
     private readonly minioService: MinioService,
     private readonly qdrantService: QdrantService,
+    private readonly configService: ConfigService,
   ) {}
 
   async createResume(file: Express.Multer.File, student_id: string) {
@@ -31,8 +33,45 @@ export class ResumesService {
       collectionName: 'resumes',
       ids: [id],
     });
-    return await this.prismaService.resume.delete({
+    const deletedResume = await this.prismaService.resume.delete({
       where: { id },
+    });
+
+    await this.minioService.deleteFile(
+      deletedResume.file_url.replace(this.configService.getMinioUrl(), ''),
+    );
+
+    return deletedResume;
+  }
+
+  async deleteResumesByStudentId(id: string) {
+    const resumes = await this.prismaService.resume.findMany({
+      where: {
+        student_id: id,
+      },
+      select: {
+        id: true,
+        file_url: true,
+      },
+    });
+
+    const ids = resumes.map((resume) => resume.id);
+
+    for (const resume of resumes) {
+      await this.minioService.deleteFile(
+        resume.file_url.replace(this.configService.getMinioUrl(), ''),
+      );
+    }
+
+    await this.qdrantService.deleteVectors({
+      collectionName: this.configService.getQdrantResumesCollection(),
+      ids,
+    });
+
+    await this.prismaService.resume.deleteMany({
+      where: {
+        student_id: id,
+      },
     });
   }
 }
